412ed58020d0e87bd1531bb5c4f52b76f2814668
EnesSiracDev <esiractopaloglu.dev@gmail.com>
2024-01-23 00:14:40 +0300

    Add project files.

---
+++ IsTheNumberPrime/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace IsTheNumberPrime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int IsItPrime = 1;
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+            if (number < 2)
+            {
+                Console.WriteLine("This number is not prime");
+            }
+            else
+            {
+                for (int i = 2; i < number; i++)
+                {
+                    if (number % i == 0)
+                    {
+                        IsItPrime++;
+                        break;
+                    }
+                }
+
+                if (IsItPrime == 1)
+                {
+                    Console.WriteLine("This number is prime");
+                }
+                else
+                {
+                    Console.WriteLine("This number is not prime");
+                }
+            }
+        }
+    }
+}
